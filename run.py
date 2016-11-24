5319dcaea0d7ccf3e79abd5b3aa1e87be35d86be
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-11-24 10:22:09 +0100

    Updated the working directory

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def main():
                      "4f3dc86f4cd29e86f05dad976b08da07b8d97d73fc8243459e09b6b208a2c8cbf6fdc7b78ae2668606388f39ef0fa715cf2" \
                      "104ad21f1846dd8f93bb25f2ce785cced2c9231466a302e5f9e0e70f72a3a912f2dae7a9a38a5e7d00eb7aef23eb42398c38" \
                      "59ffadb28ca28a1522addcaa9be4eec13095f48f7cf35".decode("HEX")
-    dispersy = Dispersy(StandaloneEndpoint(1234, '0.0.0.0'), unicode('/root/disperst_test/'), u'dispersy.db')
+    dispersy = Dispersy(StandaloneEndpoint(1234, '0.0.0.0'), unicode('.'), u'dispersy.db')
     dispersy.statistics.enable_debug_statistics(True)
     dispersy.start(autoload_discovery=True)
 
