ca147e6f84698e1e11c4943faef28569b9519bf8
Calvin Wong Loi Sing <calvin@idea-factory.nl>
2016-12-06 12:51:17 +0100

    Added the looping call

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def start_dispersy():
                  "4f3dc86f4cd29e86f05dad976b08da07b8d97d73fc8243459e09b6b208a2c8cbf6fdc7b78ae2668606388f39ef0fa715cf2" \
                  "104ad21f1846dd8f93bb25f2ce785cced2c9231466a302e5f9e0e70f72a3a912f2dae7a9a38a5e7d00eb7aef23eb42398c38" \
                  "59ffadb28ca28a1522addcaa9be4eec13095f48f7cf35".decode("HEX")
-    dispersy = Dispersy(StandaloneEndpoint(1234, '0.0.0.0'), unicode('.'), u'dispersy.db')
+    dispersy = Dispersy(StandaloneEndpoint(1235, '0.0.0.0'), unicode('.'), u'dispersy.db')
     dispersy.statistics.enable_debug_statistics(True)
     dispersy.start(autoload_discovery=True)
 
@@ -23,6 +23,8 @@ def start_dispersy():
     master_member = dispersy.get_member(public_key=master_key)
 
     community = ExampleCommunity.init_community(dispersy, master_member, my_member)
+
+    LoopingCall(lambda:community.send_example("Test", int(time.time()))).start(1.0)
 
 
 def main():
